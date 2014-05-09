c71cd1796df844cf2759b2c6e7d06f26771895fe
Tomas Harkema <info@tomasharkema.nl>
2014-05-09 23:06:15 +0200

    Safari push proof of concept

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var express = require('express'),
     mongojs = require('mongojs'),
     apns = require("apns"),
-    GitHubApi = require("github");
+    GitHubApi = require("github"),
+    socket = require('socket.io');
 
 var app = express(),
+    server = app.listen(7000),
+    io = socket.listen(server),
     options, connection, notification;
 
-app.use(express.static(__dirname + '/public'));
 
-app.listen(7000);
+io.sockets.on('connection', function(socket) {
+
+    socket.emit("notification", {
+        id: "asdfasdfasdfasf",
+        payload: {
+            title: "GithubNotifications",
+            message: "GithubNotifications"
+        }
+    });
+    setTimeout(function() {
+        socket.emit("notification", {
+            id: "asdff",
+            payload: {
+                title: "GithubNotifications",
+                message: "GithubNotifications"
+            }
+        });
+    }, 4000);
+});
+
+app.use(express.static(__dirname + '/public'));
