d783b6b527228e3d045edd806c29b326b7bf9b47
Tomas Harkema <info@tomasharkema.nl>
2014-05-10 00:13:36 +0200

    Leave some things out

--- app.js
+++ app.js
@@ -19,23 +19,9 @@ var github = new GitHubApi({
     pathPrefix: "/api/v3", // for some GHEs
     timeout: 5000
 });
-/*
-github.authorization.create({
-    scopes: ["user", "public_repo", "repo", "repo:status", "gist"],
-    note: "what this auth is for",
-    note_url: "http://gitnot.tomasharkema.nl",
-    headers: {
-        "X-GitHub-OTP": "two-factor-code"
-    }
-}, function(err, res) {
-    if (res.token) {
-        //save and use res.token as in the Oauth process above from now on
-        console.log(res);
-    }
-});
-*/
 io.sockets.on('connection', function(socket) {
 
+    /*
     socket.emit("notification", {
         id: "asdfasdfasdfasf",
         payload: {
@@ -43,15 +29,7 @@ io.sockets.on('connection', function(socket) {
             message: "GithubNotifications"
         }
     });
-    setTimeout(function() {
-        socket.emit("notification", {
-            id: "asdff",
-            payload: {
-                title: "GithubNotifications",
-                message: "GithubNotifications"
-            }
-        });
-    }, 4000);
+	*/
 });
 
 app.use(express.static(__dirname + '/public'));
